c130748098efa258d0fce39273fe2e32d105267e
kevin-lth <kevin.lethuillier@gmail.com>
2020-11-25 16:24:45 +0100

    stuff

--- catalog_api/src/app.ts
+++ catalog_api/src/app.ts
@@ -4,6 +4,10 @@ import { Movie, Genre, People } from "./types";
 import movies_json from "./catalog.json";
 
 // On crée artificiellement la liste des genres et des people - en pratique on utiliserait une BDD et ne rencontrerait donc pas ce problème
+let genres: Genre[] = [];
+let people: People[] = [];
+let movies: Movie[] = [];
+
 function convertValueToIndex<T>(reference_array: T[]) {
     return (value: T) => {
         const value_index: number = reference_array.findIndex((val: T) => val == value);
@@ -15,21 +19,25 @@ function convertValueToIndex<T>(reference_array: T[]) {
     };
 }
 
-const genres: Genre[] = [];
-const people: People[] = [];
-const movies: Movie[] = movies_json.map(movie_json => {
-    // Genres
-    const movie_genres: Genre[] = movie_json.genres;
-    movie_genres.map(convertValueToIndex(genres));
-    // People
-    const movie_cast: People[] = movie_json.cast;
-    movie_cast.map(convertValueToIndex(people));
-    movie_json.director = convertValueToIndex(people)(movie_json.director);
-    return movie_json;
-});
+function refreshBDD() {
+    genres = [];
+    people = [];
+    movies = movies_json.map(movie_json => {
+        // Genres
+        const movie_genres: Genre[] = movie_json.genres;
+        movie_genres.map(convertValueToIndex(genres));
+        // People
+        movie_json =  { ...movie_json, director: convertValueToIndex(people)(movie_json.director) };
+        const movie_cast: People[] = movie_json.cast;
+        movie_cast.map(convertValueToIndex(people));
+        return movie_json;
+    });
+}
 
 const app = express();
 app.use(express.json());
+
+refreshBDD();
 
 app.get("/movies", (req, res) => {
     res.status(200).json(movies);
@@ -50,7 +58,6 @@ app.put("/movie/:id", (req, res) => {
     const movie_index: number = movies.findIndex(movie => movie.id == id);
     if (0 <= movie_index) {
         movies[movie_index] = { ...new_movie, id }
-        const movie = movies[movie_index];
         res.status(200).json(movies[movie_index]);
     } else res.status(404).end();
 });
@@ -69,39 +76,19 @@ app.get("/genre/:id", (req, res) => {
     if (genre) res.status(200).json(genre);
     else res.status(404).end();
 });
-app.put("/genre/:id", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const genre: Genre | undefined = genres.find(genre => genre.id === id);
-    if (genre) {
-        genre.description = req.body;
-        res.status(200).json(genre);
-    } else res.status(404).end();
-});
-
 
 app.get("/peoples", (req, res) => {
-    res.status(200).json(peoples);
+    res.status(200).json(people);
 });
 app.post("/peoples", (req, res) => {
-    peoples.push(req.body);
-    res.status(200).json(peoples);
+    people.push(req.body);
+    res.status(200).json(people);
 });
 app.get("/people/:id", (req, res) => {
     const id: number = parseInt(req.params.id);
-    const people: People | undefined = peoples.find(people => people.id == id);
-    if (people) res.status(200).json(people);
+    const person: People | undefined = people.find(person => person.id == id);
+    if (person) res.status(200).json(person);
     else res.status(404).end();
 });
-app.put("/people/:id", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const new_people = req.body;
-    const people_index: number = peoples.findIndex(people => people.id == id);
-    if (0 <= people_index) {
-        peoples[people_index] = { ...new_people, id }
-        const people = peoples[people_index];
-        res.status(200).json(peoples[people_index]);
-    } else res.status(404).end();
-});
-
 
 export default app;
