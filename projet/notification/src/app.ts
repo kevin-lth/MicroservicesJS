aadc5d5c2a7dd813b9478f7baa7accf10dd22249
Kannely <blu.kannely@gmail.com>
2021-01-19 16:15:30 +0100

    squelette done

--- projet/notification/src/app.ts
+++ projet/notification/src/app.ts
@@ -6,4 +6,11 @@ import users from './users.json';
 const app = express();
 app.use(express.json());
 
+app.get("/notification/send", (req, res) => {
+    // TODO
+});
+app.post("/notification/send", (req, res) => {
+    // TODO
+});
+
 export default app;
--- projet/suggestion_film/src/app.ts
+++ projet/suggestion_film/src/app.ts
@@ -6,4 +6,18 @@ import users from './users.json';
 const app = express();
 app.use(express.json());
 
+app.get("/suggestion_film/get", (req, res) => {
+    // TODO
+});
+app.post("/suggestion_film/get", (req, res) => {
+    // TODO
+});
+
+app.get("/notification/update", (req, res) => {
+    // TODO
+});
+app.post("/notification/update", (req, res) => {
+    // TODO
+});
+
 export default app;
