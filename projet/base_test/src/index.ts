00308604cc93f2fd4717f77b5bfbd3ba945a0de2
kevin-lth <kevin.lethuillier@gmail.com>
2021-01-19 15:11:23 +0100

    docker works

--- projet/base_test/src/index.ts
+++ projet/base_test/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app'
 import { AddressInfo } from 'net'
-const server = app.listen(5000, '0.0.0.0', () => {
+const server = app.listen(8080, '0.0.0.0', () => {
     const {port, address} = server.address() as AddressInfo;
     console.log('Server listening on:','http://' + address + ':'+port);
 });
