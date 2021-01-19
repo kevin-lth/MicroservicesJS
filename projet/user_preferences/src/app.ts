e55a8091c957471c896a36988174f6fb549e5f3a
Kannely <blu.kannely@gmail.com>
2021-01-19 15:57:11 +0100

    Update app.ts

--- projet/user_preferences/src/app.ts
+++ projet/user_preferences/src/app.ts
@@ -6,47 +6,28 @@ import users from './users.json';
 const app = express();
 app.use(express.json());
 
-app.get("/users", (req, res) => {
+app.get("/user_preferences/get", (req, res) => {
     res.status(200).json(users);
 });
-app.post("/users/", (req, res) => {
+app.post("/user_preferences/get", (req, res) => {
     users.push(req.body);
     res.status(200).json(users);
 });
 
-app.get("/users/:id", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const user: User | undefined = users.find(user => user.id === id);
-    if (user) res.status(200).json(user);
-    else res.status(404).end();
+app.get("/user_preferences/update", (req, res) => {
+    res.status(200).json(users);
+});
+app.post("/user_preferences/update", (req, res) => {
+    users.push(req.body);
+    res.status(200).json(users);
 });
 
-app.get("/users/:id/playlist", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const user: User | undefined = users.find(user => user.id === id);
-    if (user) res.status(200).json(user.playlist);
-    else res.status(404).end();
+app.get("/user_preferences/delete", (req, res) => {
+    res.status(200).json(users);
 });
-app.put("/users/:id/playlist", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const user: User | undefined = users.find(user => user.id === id);
-    if (user) {
-        user.playlist = req.body;
-        res.status(200).json(user);
-    } else res.status(404).end();
-});
-
-app.get("/users/:id/suggestions", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const user: User | undefined = users.find(user => user.id === id);
-    if (user) res.status(200).json(user.suggestions);});
-app.put("/users/:id/suggestions", (req, res) => {
-    const id: number = parseInt(req.params.id);
-    const user: User | undefined = users.find(user => user.id === id);
-    if (user) {
-        user.suggestions = req.body;
-        res.status(200).json(user);
-    } else res.status(404).end();
+app.post("/user_preferences/delete", (req, res) => {
+    users.push(req.body);
+    res.status(200).json(users);
 });
 
 export default app;
