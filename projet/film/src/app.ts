99e83d0c25574ad783d1b50a205ec6d56c2ea6b6
Kannely <blu.kannely@gmail.com>
2021-01-19 17:19:58 +0100

    /film/get

--- projet/film/src/app.ts
+++ projet/film/src/app.ts
@@ -1,13 +1,26 @@
 import express from 'express';
+const mysql = require('mysql');
 
-import users from './users.json';
 
 const app = express();
 app.use(express.json());
 
+const db = mysql.createConnection({
+    host: "localhost", 
+    user: "nom_utilisateur",
+    password: "mot_de_passe_utilisateur" });
+
 app.get("/film/get", (req, res) => {
     const id: number = parseInt(req.params.id);
-    //TODO
+    db.connect(function(err: any) {
+        if (err) throw err;
+        console.log("Connecté à la base de données MySQL!");
+        var sql = "SELECT * FROM film WHERE id = ?";
+        db.query(sql, id, function(err: any, result: any) {
+            if (err) throw err;
+            console.log(result)
+        });
+    });
 });
 
 app.get("/film/search", (req, res) => {
